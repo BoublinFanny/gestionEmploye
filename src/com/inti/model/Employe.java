f840ad92d719289b965e4727a3898ce8e4ca9f3f
FannyBoublin <fanny.boublin@gmail.com>
2022-11-14 14:51:25 +0100

    version avec erreur

--- src/com/inti/model/Employe.java
+++ src/com/inti/model/Employe.java
@@ -1,5 +1,46 @@
 package com.inti.model;
 
 public class Employe {
+	private long idEmploye;
+	private String nomEmploye;
+	private int age;
+	private float salaireeeeee;
+	
+	private Employe() {
+		
+	}
 
+	public long getIdEmploye() {
+		return idEmploye;
+	}
+
+	public void setIdEmploye(long idEmploye) {
+		this.idEmploye = idEmploye;
+	}
+
+	public String getNomEmploye() {
+		return nomEmploye;
+	}
+
+	public void setNomEmploye(String nomEmploye) {
+		this.nomEmploye = nomEmploye;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public float getSalaire() {
+		return salaire;
+	}
+
+	public void setSalaire(float salaire) {
+		this.salaire = salaire;
+	}
+	
+	
 }
